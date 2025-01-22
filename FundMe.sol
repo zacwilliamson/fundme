5092669b95d86aabd1cc053e11df06b0575566a4
zacwilliamson <zacwilliamson@icloud.com>
2025-01-22 15:32:52 -0600

    base FundMe contract

---
+++ FundMe.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Get funds from user
+// Withdraw funds
+// Set a minimum funding value in USD'
+
+contract FundMe {
+    function fund() public {
+
+    }
+
+    function withdraw() public {
+        
+    }
+}
