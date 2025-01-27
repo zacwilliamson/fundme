790b5fd0b32c2b65b009f75971a739829050e31d
zacwilliamson <zacwilliamson@icloud.com>
2025-01-27 16:59:15 -0600

    added modifier and constructor

--- FundMe.sol
+++ FundMe.sol
@@ -16,13 +16,33 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256 amountFunded) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 wei = 1 * 10 ** 18
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
+        for(uint256 i = 0; i < funders.length; i++){
+            address funder = funders[i];
+            addressToAmountFunded[funder]= 0;
+        }
 
+        funders = new address[](0);
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner");
+        _;
     }
 }
